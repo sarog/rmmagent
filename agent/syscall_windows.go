9f2298580cdbf1929b317ce0a501a4efa3bb235d
Saro G <sarog@users.noreply.github.com>
2022-11-30 17:44:50 -0500

    Replaced deprecated Syscall() functions

--- agent/syscall_windows.go
+++ agent/syscall_windows.go
@@ -56,7 +56,7 @@ const (
 )
 
 func FormatMessage(flags uint32, source syscall.Handle, messageID uint32, languageID uint32, buffer *byte, bufferSize uint32, arguments uintptr) (numChars uint32, err error) {
-	r0, _, e1 := syscall.Syscall9(procFormatMessageW.Addr(), 7, uintptr(flags), uintptr(source), uintptr(messageID), uintptr(languageID), uintptr(unsafe.Pointer(buffer)), uintptr(bufferSize), uintptr(arguments), 0, 0)
+	r0, _, e1 := syscall.SyscallN(procFormatMessageW.Addr(), 7, uintptr(flags), uintptr(source), uintptr(messageID), uintptr(languageID), uintptr(unsafe.Pointer(buffer)), uintptr(bufferSize), uintptr(arguments), 0, 0)
 	numChars = uint32(r0)
 	if numChars == 0 {
 		if e1 != 0 {
@@ -69,7 +69,7 @@ func FormatMessage(flags uint32, source syscall.Handle, messageID uint32, langua
 }
 
 func GetOldestEventLogRecord(eventLog w32.HANDLE, oldestRecord *uint32) (err error) {
-	r1, _, e1 := syscall.Syscall(procGetOldestEventLogRecord.Addr(), 2, uintptr(eventLog), uintptr(unsafe.Pointer(oldestRecord)), 0)
+	r1, _, e1 := syscall.SyscallN(procGetOldestEventLogRecord.Addr(), 2, uintptr(eventLog), uintptr(unsafe.Pointer(oldestRecord)), 0)
 	if r1 == 0 {
 		if e1 != 0 {
 			err = error(e1)
@@ -81,7 +81,7 @@ func GetOldestEventLogRecord(eventLog w32.HANDLE, oldestRecord *uint32) (err err
 }
 
 func LoadLibraryEx(filename *uint16, file syscall.Handle, flags uint32) (handle syscall.Handle, err error) {
-	r0, _, e1 := syscall.Syscall(procLoadLibraryExW.Addr(), 3, uintptr(unsafe.Pointer(filename)), uintptr(file), uintptr(flags))
+	r0, _, e1 := syscall.SyscallN(procLoadLibraryExW.Addr(), 3, uintptr(unsafe.Pointer(filename)), uintptr(file), uintptr(flags))
 	handle = syscall.Handle(r0)
 	if handle == 0 {
 		if e1 != 0 {
@@ -94,7 +94,7 @@ func LoadLibraryEx(filename *uint16, file syscall.Handle, flags uint32) (handle 
 }
 
 func ReadEventLog(eventLog w32.HANDLE, readFlags ReadFlag, recordOffset uint32, buffer *byte, numberOfBytesToRead uint32, bytesRead *uint32, minNumberOfBytesNeeded *uint32) (err error) {
-	r1, _, e1 := syscall.Syscall9(procReadEventLogW.Addr(), 7, uintptr(eventLog), uintptr(readFlags), uintptr(recordOffset), uintptr(unsafe.Pointer(buffer)), uintptr(numberOfBytesToRead), uintptr(unsafe.Pointer(bytesRead)), uintptr(unsafe.Pointer(minNumberOfBytesNeeded)), 0, 0)
+	r1, _, e1 := syscall.SyscallN(procReadEventLogW.Addr(), 7, uintptr(eventLog), uintptr(readFlags), uintptr(recordOffset), uintptr(unsafe.Pointer(buffer)), uintptr(numberOfBytesToRead), uintptr(unsafe.Pointer(bytesRead)), uintptr(unsafe.Pointer(minNumberOfBytesNeeded)), 0, 0)
 	if r1 == 0 {
 		if e1 != 0 {
 			err = error(e1)
